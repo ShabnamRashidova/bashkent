7b083b228062054b79f596bbc6ebecb13f17e4ee
shebnem reshidova <shabnamrashidova90@gmail.com>
2022-07-18 17:50:18 +0400

    0

--- js/script.js
+++ js/script.js
@@ -1,100 +1,122 @@
 $('.banner-carousel').owlCarousel({
-    loop:false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    loop: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+
         },
-        
-        1000:{
-            items:1,
-            
-            loop:false
+
+        1000: {
+            items: 1,
+
+            loop: false
         }
     }
 })
 $('.branches-carousel').owlCarousel({
-    loop:false,
-    margin:20,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    loop: false,
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+
         },
-        
-        1000:{
-            items:3,
-            
-            loop:false
+600:{
+    items:2,
+},
+        1000: {
+            items: 3,
+
+            loop: false
         }
     }
 })
 $('.partners-carousel').owlCarousel({
-    loop:false,
-    margin:40,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    loop: false,
+    margin: 40,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+
         },
-        
-        1000:{
-            items:5,
-            
-            loop:false
-        }
+        700: {
+            items: 2
+        },
+        900: {
+            items: 3,
+        },
+        1200: {
+            items: 5,
+
+            loop: false
+        },
+
     }
 })
 $('.blog-carousel').owlCarousel({
-    loop:false,
-    margin:40,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        
-        1000:{
-            items:3,
-            
-            loop:false
-        }
+    loop: false,
+    margin: 20,
+    responsiveClass: true,
+    autoPlay: true,
+    responsive: {
+     0:{
+        items:1,
+     },
+    700:{
+        items:2,
+     },
+     1000:{
+        items:3
+     }
     }
 })
 $('.testimonial-carousel').owlCarousel({
-    loop:false,
-    margin:40,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    loop: false,
+    margin: 40,
+
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+
         },
-        
-        1000:{
-            items:1,
-            
-            loop:false
+
+        1000: {
+            items: 1,
+
+            loop: false
         }
     }
 })
-$(".nav-menu-btn").on("click",()=>{
+$(".nav-menu-btn").on("click", () => {
     $(".header-bottom-content").addClass("opened")
 });
-$(".menu-close-icon").on("click",()=>{
+$(".menu-close-icon").on("click", () => {
     $(".header-bottom-content").removeClass("opened");
 });
-$(".bg-overlay").on("click",()=>{
+$(".bg-overlay").on("click", () => {
     $(".header-bottom-content").removeClass("opened");;
 });
-$(".search-btn").on("click",(e)=>{
+$(".search-btn").on("click", (e) => {
     $(".search-btn .fa-solid").toggleClass("fa-magnifying-glass");
     $(".search-btn .fa-solid").toggleClass("fa-times")
-$(".search-input").toggleClass("show");
-e.preventDefault();
-console.log("ewe")
+    $(".search-input").toggleClass("show");
+    e.preventDefault();
+    console.log("ewe")
+});
+// Fancybox Config
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+        "slideShow",
+        "thumbs",
+        "zoom",
+        "fullScreen",
+        "share",
+        "close"
+    ],
+    loop: false,
+    protect: true
 });
