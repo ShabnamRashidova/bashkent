d421131a59e6bf888e03bf0eb6bb93f664a36a1c
shebnem reshidova <shabnamrashidova90@gmail.com>
2022-07-20 09:29:30 +0400

    ...

---
+++ js/category.js
@@ -0,0 +1,21 @@
+const items = document.querySelector(".items");
+const product_item = document.querySelectorAll(".product-item");
+items.onclick = (selectedItem) => {
+
+    if (selectedItem.target.classList.contains("item")) {
+
+        selectedItem.target.classList.add("active");
+        items.querySelector(".active").classList.remove("active");
+        let filterItems = selectedItem.target.getAttribute("data-name");
+        product_item.forEach(item => {
+            let filterProducts = item.getAttribute("data-name");
+            if (filterItems == filterProducts) {
+                item.classList.add("show");
+                item.classList.remove("hide");
+            } else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        })
+    }
+}
--- js/script.js
+++ js/script.js
@@ -120,24 +120,4 @@ $('[data-fancybox="gallery"]').fancybox({
     loop: false,
     protect: true
 });
-const items = document.querySelector(".items");
-const product_item = document.querySelectorAll(".product-item");
-items.onclick = (selectedItem) => {
 
-    if (selectedItem.target.classList.contains("item")) {
-     
-        selectedItem.target.classList.add("active");
-        items.querySelector(".active").classList.remove("active"); 
-        let filterItems = selectedItem.target.getAttribute("data-name");
-        product_item.forEach(item => {
-            let filterProducts = item.getAttribute("data-name");
-            if (filterItems == filterProducts) {
-                item.classList.add("show");
-                item.classList.remove("hide");
-            } else {
-                item.classList.remove("show");
-                item.classList.add("hide");
-            }
-        })
-    }
-}
