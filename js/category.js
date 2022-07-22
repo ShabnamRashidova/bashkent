e41099ae9cc572867cdcc45a25ca515f63b84f7d
shebnem reshidova <shabnamrashidova90@gmail.com>
2022-07-22 12:48:16 +0400

    4

--- js/category.js
+++ js/category.js
@@ -1,11 +1,12 @@
 const items = document.querySelector(".items");
 const product_item = document.querySelectorAll(".product-item");
 items.onclick = (selectedItem) => {
-console.log("err")
-    if (selectedItem.target.classList.contains("item")) {
 
-        selectedItem.target.classList.add("active");
+    if (selectedItem.target.classList.contains("item")) {
         items.querySelector(".active").classList.remove("active");
+        selectedItem.target.classList.add("active");
+     
+       
         let filterItems = selectedItem.target.getAttribute("data-name");
         product_item.forEach(item => {
             let filterProducts = item.getAttribute("data-name");
